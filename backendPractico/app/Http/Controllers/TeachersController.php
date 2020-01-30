b3a1ea0f58118195409f7be261dd95543d20bb56
Manuel Baranosky <manuel.baranosky@gmail.com>
2020-01-29 22:22:05 -0300

    inicio de base de datos y api

---
+++ backendPractico/app/Http/Controllers/TeachersController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Http\Request;
+use App\Teacher;
+
+class TeachersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $teachers = Teacher::get();
+        echo json_encode($teachers);
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $teacher = new Teacher();
+        $teacher -> userName = $request -> input('userName');
+        $teacher -> email = $request -> input('email');
+        $teacher -> password = $request -> input('password');
+        $teacher -> save();
+
+        echo json_encode($teacher);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
